1c3fa66cbda64d68840f482aa6a15b6da2156f55
oumao <evansotis2015@gmail.com>
2021-11-30 17:24:20 +0300

    Added item

--- js/kanban.js
+++ js/kanban.js
@@ -1,22 +1,24 @@
-let order = 1;
-let adding = false;
+let order = 1
+let adding = false
 
-const error = document.querySelector('.error');
-const message = 'Please add a description.';
+const error = document.querySelector(".error")
+const message = "Please add a description."
 
-const add_btn = document.querySelector('.add');
-add_btn.addEventListener('click', () => {
-  const target = document.querySelector('#requested');
+const add_btn = document.querySelector(".add")
+add_btn.addEventListener("click", () => {
+  const target = document.querySelector("#requested")
   if (adding == false) {
-    adding = true;
-    target.appendChild(create_item());
+    adding = true
+    target.appendChild(create_item())
   } else {
-    error.innerHTML = message;
+    error.innerHTML = message
   }
-});
+})
 
 const create_item = () => {
-};
+  let item = document.createElement("div")
+  item.classList.add("item")
+  item.id = "item-"+ order
+}
 
-document.querySelectorAll('.drop').forEach(element => {
-});
+document.querySelectorAll(".drop").forEach((element) => {})
